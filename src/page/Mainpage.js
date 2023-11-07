2df352a29428fe54a39737ea5705bb6ed720eb9a
Hwang-Jeong-Seon <jshwang370@gmail.com>
2023-11-07 11:24:54 +0900

    #1 react redux, reducer, middleware사용하는 방향으로 로직 고도화 작업

--- src/page/Mainpage.js
+++ src/page/Mainpage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 
 import ProductCard from "../component/ProductCard";
 import {Container, Row, Col} from "react-bootstrap";
